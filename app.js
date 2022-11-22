1529b70f98f6f8d0483e098eafc1bfd780b547b3
Shiba-Patrick <liu21407@gmail.com>
2022-11-22 17:52:07 +0800

    mongoose connect set and other npm

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 const express = require('express')
 const express_hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 const PORT = 3000
 const app = express()
 
-//setting handlebars
-app.engine('handlebars',express_hbs.engine({defaultLayout: 'main'}))
+//setting handlebars/bodyParser/method-override
+app.engine('handlebars', express_hbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
+//非production環境使用doten
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+//mongoDB setting
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('MongoDB error')
+})
+
+db.once('open', () => {
+  console.log('MongoDB connect!!!')
+})
+
+//homepage
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+//app.listen
 app.listen(PORT, () => {
   console.log(`App is listen on localhost:${PORT}`)
 })
